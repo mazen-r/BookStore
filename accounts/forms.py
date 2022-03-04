6648f9f507ebd26552a65150eb09dab3b3bbb654
Mazen Ramadan <mazenramadan00@gmail.com>
2022-03-04 18:07:28 +0200

    Users

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
-class CustomUserCreation(UserCreationForm):
+class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = get_user_model()
         fields = ('email', 'username')
