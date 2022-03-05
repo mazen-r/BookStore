3ea040dcf1790721aa269dafbdad5c734b3e4df4
Mazen Ramadan <mazenramadan00@gmail.com>
2022-03-05 22:30:47 +0200

    User Unit-Testing

--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Django admin
     path('admin/', admin.site.urls),
-    path('', include('pages.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/', include('accounts.urls'))
 
+    # User management
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    # Local apps
+    path('accounts/', include('accounts.urls')), # new
+    path('', include('pages.urls')),
 ]
