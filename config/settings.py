e6596041e6ea3ff8873a0d5cd04cd85ff744cfb9
Mazen Ramadan <mazenramadan00@gmail.com>
2022-03-08 22:43:37 +0200

    Book Cover

--- config/settings.py
+++ config/settings.py
@@ -138,3 +138,6 @@ ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True 
 
 DEFAULT_FROM_EMAIL = 'admin@djangobookstore.com'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
